2cd708bfa0cfba413bb0b2eadbc5c891c6040a57
lucho1 <luchosuaya@hotmail.com>
2020-03-06 20:58:43 +0100

    Improved Shader readability & size

--- CronosEngine/Cronos/res/Shaders/basic.glsl
+++ CronosEngine/Cronos/res/Shaders/basic.glsl
@@ -33,6 +33,7 @@ void main()
 #type fragment
 #version 430 core
 
+// SHADER ASSERTION -----------------------------------------------------------------------------------------------------------------------------
 #define CNSH_ASSERT(condition) while(!condition) { ++m_AssertTimes; break; }
 #define COLOROUTPUT_EXIT() while(m_AssertTimes != 0) { color = AssertColorOutput(); break; }
 
@@ -47,78 +48,16 @@ vec4 AssertColorOutput()
 	blinkColor = vec4(randomNum(u_ShaderPlaybackTime), randomNum(u_ShaderPlaybackTime/2), randomNum(u_ShaderPlaybackTime/3), 1.0);
 	return (mod(u_ShaderPlaybackTime, 1.0) > 0.5) ? blinkColor : vec4(1.0);
 }
+// ----------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#define MAX_POINTLIGHTS 20
-#define MAX_SPOTLIGHTS 20
-#define MAX_DIRLIGHTS 8
-
+// SHADER ITSELF --------------------------------------------------------------------------------------------------------------------------------
 //Input variables
 in vec2 v_TexCoords;
 in vec3 v_Normal;
 in vec3 v_FragPos;
 in vec3 v_CamPos;
 
-//Output variables
-out vec4 color;
-
-//Lights Structures
-struct DirLight 
-{
-	vec4 LightDir;
-	vec4 LightColor;
-
-	float LightIntensity;
-};
-
-struct PointLight
-{
-	vec4 LightPos;
-	vec4 LightColor;
-
-	float LightIntensity;
-
-	float LightAtt_K;
-	float LightAtt_L;
-	float LightAtt_Q;
-};
-
-struct SpotLight
-{
-	vec4 LightPos;
-	vec4 LightColor;
-	vec4 LightDir;
-
-	float LightIntensity;
-
-	float LightAtt_K;
-	float LightAtt_L;
-	float LightAtt_Q;
-
-	float cutoffAngleCos;
-	float outerCutoffAngleCos;
-};
-
-//Actual Lights
-layout(std430, binding = 0) buffer SSBOData
-{
-	int u_CurrentLightsNum[3];
-	PointLight u_PointLightsArray[MAX_POINTLIGHTS];
-};
-
-layout(std430, binding = 1) buffer SSBOData2
-{
-	DirLight u_DirLightsArray[MAX_DIRLIGHTS];
-};
-
-layout(std430, binding = 2) buffer SSBOData3
-{	
-	SpotLight u_SPLightsArray[MAX_SPOTLIGHTS];
-};
-
-//uniform DirLight u_DirLightsArray[MAX_DIRLIGHTS] = DirLight[MAX_DIRLIGHTS](DirLight(vec3(0), vec3(1), 0.0));
-//uniform PointLight u_PointLightsArray[MAX_POINTLIGHTS] = PointLight[MAX_POINTLIGHTS](PointLight(vec3(0), vec3(1), 0.0, 1.0, 0.09, 0.032));
-//uniform SpotLight u_SPLightsArray[MAX_SPOTLIGHTS];
 
 //Material Stuff
 uniform vec4 u_AmbientColor;
@@ -130,38 +69,72 @@ uniform sampler2D u_SpecularTexture;
 
 uniform bool u_TextureEmpty = true;
 
-//ZBuffer rendering -------------------------------------------------------------------------------------------------
-uniform vec2 u_CamPlanes; //x for near plane, y for far plane
-uniform bool u_drawZBuffer = false;
 
-float LinearizeZ(float depth)
+//Lights Structures
+struct DirLight 
 {
-	float z = depth*2.0 - 1.0;
-	return (2.0*u_CamPlanes.x*u_CamPlanes.y)/(u_CamPlanes.y + u_CamPlanes.x - z*(u_CamPlanes.y - u_CamPlanes.x));
-}
+	vec4 LightDir;
+	vec4 LightColor;
+	float LightIntensity;
+};
 
-//Light Calculations -----------------------------------------------------------------------------------------------
-vec3 CalculateDiffSpecLightResult(bool hasTextures, vec3 LColor, float diff, float spec)
+struct PointLight
 {
-	//Result
-	if(hasTextures)
-	{
-		vec3 diffuse = LColor * diff * texture(u_DiffuseTexture, v_TexCoords).rgb;
-		vec3 specular = LColor * spec * texture(u_SpecularTexture, v_TexCoords).rgb;
+	vec4 LightPos;
+	vec4 LightColor;
+	float LightIntensity;
 
-		return (diffuse + specular);
-	}
-	else
-	{
-		vec3 diffuse = LColor * diff;
-		vec3 specular = LColor * spec;
+	float LightAtt_K, LightAtt_L, LightAtt_Q; //Attenuation Factors (K constant, L linear, Q quadratic)
+};
 
-		return (diffuse + specular);
-	}
+struct SpotLight
+{
+	vec4 LightPos;
+	vec4 LightColor;
+	vec4 LightDir;
+	float LightIntensity;
+
+	float LightAtt_K, LightAtt_L, LightAtt_Q; //Attenuation Factors (K constant, L linear, Q quadratic)
+	float innerCutoffAngleCos, outerCutoffAngleCos; //Inner/Outer Cutoff values for light effect
+};
+
+//Lights Shader Storage Buffers
+layout(std430, binding = 0) buffer SSBOData
+{
+	int u_CurrentLightsNum[3];
+	PointLight u_PointLightsArray[];
+};
+
+layout(std430, binding = 1) buffer SSBOData2
+{
+	DirLight u_DirLightsArray[];
+};
+
+layout(std430, binding = 2) buffer SSBOData3
+{	
+	SpotLight u_SPLightsArray[];
+};
+
+
+//Light Calculations Functions ---------------------------------------------------------------------------------------
+vec3 CalculateLightResult(vec3 LColor, float diff, float spec)
+{
+	//Calculate light result
+	vec3 diffuse = LColor * diff;
+	vec3 specular = LColor * spec;
+
+	//If we have textures, apply them
+	if(!u_TextureEmpty)
+	{
+		diffuse *= texture(u_DiffuseTexture, v_TexCoords).rgb;
+		specular *= texture(u_SpecularTexture, v_TexCoords).rgb;
+	}	
+
+	return (diffuse + specular);
 }
 
 //Dir Light Calculation
-vec3 CalculateDirectionalLight(DirLight dLight, vec3 normal, vec3 viewDirection, bool hasTextures)
+vec3 CalculateDirectionalLight(DirLight dLight, vec3 normal, vec3 viewDirection)
 {
 	vec3 lightDir = normalize(dLight.LightDir.xyz);
 	
@@ -173,13 +146,13 @@ vec3 CalculateDirectionalLight(DirLight dLight, vec3 normal, vec3 viewDirection,
 	float specImpact = pow(max(dot(normal, halfwayDir), 0.0), u_Shininess);
 
 	//Result
-	return CalculateDiffSpecLightResult(hasTextures, dLight.LightColor.xyz, diffImpact, specImpact) * dLight.LightIntensity;
+	return CalculateLightResult(dLight.LightColor.xyz, diffImpact, specImpact) * dLight.LightIntensity;
 }
 
 //Point Light Calculation
-vec3 CalculatePointLight(PointLight pLight, vec3 normal, vec3 FragPos, vec3 viewDirection, bool hasTextures)
+vec3 CalculatePointLight(PointLight pLight, vec3 normal, vec3 viewDirection)
 {
-	vec3 lightDir = normalize(pLight.LightPos.xyz - FragPos);
+	vec3 lightDir = normalize(pLight.LightPos.xyz - v_FragPos);
 
 	//Diffuse Component
 	float diffImpact = max(dot(normal, lightDir), 0.0);
@@ -189,17 +162,17 @@ vec3 CalculatePointLight(PointLight pLight, vec3 normal, vec3 FragPos, vec3 view
 	float specImpact = pow(max(dot(normal, halfwayDir), 0.0), u_Shininess);
 
 	//Attenuation Calculation
-	float d = length(pLight.LightPos.xyz - FragPos);
+	float d = length(pLight.LightPos.xyz - v_FragPos);
 	float lightAttenuation = 1.0/ (pLight.LightAtt_K + pLight.LightAtt_L * d + pLight.LightAtt_Q *(d * d));
 
 	//Result
-	return CalculateDiffSpecLightResult(hasTextures, pLight.LightColor.xyz, diffImpact, specImpact) * lightAttenuation * pLight.LightIntensity;
+	return CalculateLightResult(pLight.LightColor.xyz, diffImpact, specImpact) * lightAttenuation * pLight.LightIntensity;
 }
 
 //Spot Light Calculation
-vec3 CalculateSpotLight(SpotLight spLight, vec3 normal, vec3 FragPos, vec3 viewDirection, bool hasTextures)
+vec3 CalculateSpotLight(SpotLight spLight, vec3 normal, vec3 viewDirection)
 {
-	vec3 lightDir = normalize(spLight.LightPos.xyz - FragPos);	
+	vec3 lightDir = normalize(spLight.LightPos.xyz - v_FragPos);	
 	
 	//Diffuse Component
 	float diffImpact = max(dot(normal, lightDir), 0.0);
@@ -210,21 +183,36 @@ vec3 CalculateSpotLight(SpotLight spLight, vec3 normal, vec3 FragPos, vec3 viewD
 
 	//Spotlight Calcs for Soft Edges
 	float theta = dot(lightDir, normalize(-spLight.LightDir.xyz));
-	float epsilon = spLight.cutoffAngleCos - spLight.outerCutoffAngleCos;
+	float epsilon = spLight.innerCutoffAngleCos - spLight.outerCutoffAngleCos;
 	float lightIntensity = clamp((theta - spLight.outerCutoffAngleCos) / epsilon, 0.0, 1.0) * spLight.LightIntensity;
 
 	//Attenuation Calculation
-	float d = length(spLight.LightPos.xyz - FragPos);
+	float d = length(spLight.LightPos.xyz - v_FragPos);
 	float lightAttenuation = 1.0/ (spLight.LightAtt_K + spLight.LightAtt_L * d + spLight.LightAtt_Q *(d * d));
 
 	//Result
-	return CalculateDiffSpecLightResult(hasTextures, spLight.LightColor.xyz, diffImpact, specImpact) * lightIntensity * lightAttenuation;
+	return CalculateLightResult(spLight.LightColor.xyz, diffImpact, specImpact) * lightIntensity * lightAttenuation;
+}
+
+
+//ZBuffer rendering -------------------------------------------------------------------------------------------------
+uniform vec2 u_CamPlanes; //x for near plane, y for far plane
+uniform bool u_drawZBuffer = false;
+
+float LinearizeZ(float depth)
+{
+	float z = depth * 2.0 - 1.0;
+	return (2.0 * u_CamPlanes.x * u_CamPlanes.y) / (u_CamPlanes.y + u_CamPlanes.x - z*(u_CamPlanes.y - u_CamPlanes.x));
 }
 
 //------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------
+//Output variables
+out vec4 color;
+
 void main()
 {
+	//Drawing ZBuffer (if we must)
 	if (u_drawZBuffer)
 	{
 		float depth = (LinearizeZ(gl_FragCoord.z) / u_CamPlanes.y);
@@ -232,9 +220,10 @@ void main()
 	}
 	else
 	{
-		CNSH_ASSERT((u_CurrentLightsNum[0] <= MAX_DIRLIGHTS && u_CurrentLightsNum[0] >= 0));
-		CNSH_ASSERT((u_CurrentLightsNum[1] <= MAX_POINTLIGHTS && u_CurrentLightsNum[1] >= 0));
-		CNSH_ASSERT((u_CurrentLightsNum[2] <= MAX_SPOTLIGHTS && u_CurrentLightsNum[2] >= 0));
+		//Initial Assertion Check
+		CNSH_ASSERT((u_CurrentLightsNum[0] >= 0));
+		CNSH_ASSERT((u_CurrentLightsNum[1] >= 0));
+		CNSH_ASSERT((u_CurrentLightsNum[2] >= 0));
 
 		//Generic Light Calculations
 		vec3 normalVec = normalize(v_Normal);
@@ -243,20 +232,23 @@ void main()
 		//Color Output
 		vec3 colorOutput = vec3(0.0);
 
+		//Lighting Calculations
 		for(int i = 0; i < u_CurrentLightsNum[0]; ++i)
-			colorOutput += CalculateDirectionalLight(u_DirLightsArray[i], normalVec, viewDirection, !u_TextureEmpty);
+			colorOutput += CalculateDirectionalLight(u_DirLightsArray[i], normalVec, viewDirection);
 
 		for(int i = 0; i < u_CurrentLightsNum[1]; ++i)
-			colorOutput += CalculatePointLight(u_PointLightsArray[i], normalVec, v_FragPos, viewDirection, !u_TextureEmpty);
+			colorOutput += CalculatePointLight(u_PointLightsArray[i], normalVec, viewDirection);
 
 		for(int i = 0; i < u_CurrentLightsNum[2]; ++i)
-			colorOutput += CalculateSpotLight(u_SPLightsArray[i], normalVec, v_FragPos, viewDirection, !u_TextureEmpty);
-
+			colorOutput += CalculateSpotLight(u_SPLightsArray[i], normalVec, viewDirection);
+		
+		//Texturing Calculations & Final Fragment Color
 		if(!u_TextureEmpty)
 			color = vec4(colorOutput + u_AmbientColor.rgb * texture(u_DiffuseTexture, v_TexCoords).rgb, 1.0);
 		else
 			color = vec4(colorOutput + u_AmbientColor.rgb, 1.0);
 
+		//Final Assertion Check
 		COLOROUTPUT_EXIT();
 	}
 }		
